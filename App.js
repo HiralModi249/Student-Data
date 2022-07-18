3f1fbca21887f2178df32b76436f7223df51749b
hiral-modi <hmodi3@uic.edu>
2022-07-18 16:44:01 -0500

    tracking changes

--- App.js
+++ App.js
@@ -13,57 +13,82 @@ db.connect(function(err) {
     console.log("Connected!");  
 });
 
+// let query1 = 
+// "select * from BTT_TEST.student_semester_data where  `Primary Curr Major Name` in ('Data Science', 'Computer Science', 'Math & Computer Science', 'Math and Computer Science', 'Computer Sci & Linguistics', 'Liberal Arts - Pre-CS & Ling') or   `Primary Curr Minor 1 Name` in ('Data Science', 'Computer Science', 'Math & Computer Science', 'Math and Computer Science', 'Computer Sci & Linguistics', 'Liberal Arts - Pre-CS & Ling') or   `Primary Curr Minor 2 Name` in ('Data Science', 'Computer Science', 'Math & Computer Science', 'Math and Computer Science', 'Computer Sci & Linguistics', 'Liberal Arts - Pre-CS & Ling') " ;
+
 
 let courses = ["Computer Sci & Linguistics", "Computer Science", 
                     "Engineering - Pre-CS & Design", 
                     "Engineering - Pre-CS & Data Science",
                      "Data Science" ];
 
-function trackChange(track) {
-  for (let i =0; i<track.length; i++) {
-    for (let j =1; j<track.length-1; j++) {
-      for (let k =0; k<6; k++) {
-        if((courses[k] == track[i]['Primary Curr Major Name']) && (courses[k] != track[j]['Primary Curr Major Name'])) {
-          track[i] = {UIN: track[i].UIN, Term: track[i]['Term Code'], Major: track[i]['Primary Curr Major Name'],
-                    ChangedTerm: track[j]['Term Code'], ChangedMajor: track[j]['Primary Curr Major Name']}
-          //console.log('here');
-        }
-        if((courses[k] == track[i]['Primary Curr Minor 1 Name']) && (courses[k] != track[j]['Primary Curr Minor 1 Name'])) {
-          track[i] = {UIN: track[i].UIN, Term: track[i]['Term Code'], Minor_1: track[i]['Primary Curr Minor 1 Name'],
-                    ChangedTerm: track[j]['Term Code'], ChangedMinor_1: track[j]['Primary Curr Minor 1 Name']}
-          //console.log('here');
-        }
-        if((courses[k] == track[i]['Primary Curr Minor 2 Name']) && (courses[k] != track[j]['Primary Curr Minor 2 Name'])) {
-          track[i] = {UIN: track[i].UIN, Term: track[i]['Term Code'], Minor_2: track[i]['Primary Curr Minor 2 Name'],
-                    ChangedTerm: track[j]['Term Code'], ChangedMinor_2: track[j]['Primary Curr Minor 2 Name']}
-          //console.log('here');
-        }
-        else {
-          console.log('NO STUDENT CHANGED THEIR MAJOR OR MINOR');
-        }
-      }
-    }
-  }
-  return track;
-}
-
-// let query1 = 
-// "select * from BTT_TEST.student_semester_data where  `Primary Curr Major Name` in ('Data Science', 'Computer Science', 'Math & Computer Science', 'Math and Computer Science', 'Computer Sci & Linguistics', 'Liberal Arts - Pre-CS & Ling') or   `Primary Curr Minor 1 Name` in ('Data Science', 'Computer Science', 'Math & Computer Science', 'Math and Computer Science', 'Computer Sci & Linguistics', 'Liberal Arts - Pre-CS & Ling') or   `Primary Curr Minor 2 Name` in ('Data Science', 'Computer Science', 'Math & Computer Science', 'Math and Computer Science', 'Computer Sci & Linguistics', 'Liberal Arts - Pre-CS & Ling') " ;
-
+var temp = [];
+let countMajor =0;
+let countMinor=0;
 let query1 = "select * from BTT_TEST.student_semester_data";
 db.query(query1, function (err, result, fields) {
-    if (err) throw err;
-    fs.writeFileSync('Tech_Discipline.json', JSON.stringify(result,undefined,2));
-    
-    //let t = Object.getOwnPropertyNames(result)    
-    for (let i =0; i<result.length; i++) {
-      if (result[i].UIN == result[i+1].UIN) {
-        console.log(trackChange(result));
+  if (err) throw err;
+  fs.writeFileSync('Tech_Discipline.json', JSON.stringify(result,undefined,2));
+  //let keys = Object.getOwnPropertyNames(result)    
+  for (let i =0, j=1; i<result.length, j<result.length-1; i++, j++) {
+    if (result[i].UIN == result[j].UIN) {
+      for (let k =0; k<6; k++) {
+        
+        //FOR CHANGES IN MAJOR
+        //Comparing major of one to another - if UIN matches and major does not match then UIN, major and changed major gets push into temp
+        if(((courses[k] == result[i]['Primary Curr Major Name']) && (courses[k] != result[j]['Primary Curr Major Name'])) ||
+            ((courses[k] != result[i]['Primary Curr Major Name']) && (courses[k] == result[j]['Primary Curr Major Name']))    ){  
+            // temp.push({UIN: result[i].UIN, Term: result[i]['Term Code'], Major: result[i]['Primary Curr Major Name'],
+            //         ChangedTerm: result[j]['Term Code'], ChangedMajor: result[j]['Primary Curr Major Name']})
+            countMajor++;
+        }
+
+      //Comparing minor of one to another - if UIN matches and minor does not match then UIN, minor and changed minor gets push into temp
+        //FOR CHANGES IN MINOR 1  
+        if(((courses[k] == result[i]['Primary Curr Minor 1 Name']) && (courses[k] != result[j]['Primary Curr Minor 1 Name'])) ||
+            ((courses[k] != result[i]['Primary Curr Minor 1 Name']) && (courses[k] == result[j]['Primary Curr Minor 1 Name']))    ){
+            // temp.push({UIN: result[i].UIN, Term: result[i]['Term Code'], Minor1: result[i]['Primary Curr Minor 1 Name'],
+            //         ChangedTerm: result[j]['Term Code'], ChangedMINOR_1: result[j]['Primary Curr Minor 1 Name']})
+            countMinor++;
+        }
+
+        //FOR CHANGES IN MINOR 2
+        if(((courses[k] == result[i]['Primary Curr Minor 2 Name']) && (courses[k] != result[j]['Primary Curr Minor 2 Name'])) ||
+            ((courses[k] != result[i]['Primary Curr Minor 2 Name']) && (courses[k] == result[j]['Primary Curr Minor 2 Name']))    ){
+            // temp.push({UIN: result[i].UIN, Term: result[i]['Term Code'], Minor2: result[i]['Primary Curr Minor 2 Name'],
+            //         ChangedTerm: result[j]['Term Code'], ChangedMINOR_2: result[j]['Primary Curr Minor 2 Name']})
+            countMinor++;
+        }
+
       }
-    }  
-    
+    }
+  }  
+  //console.log(temp);
+  console.log("countMajor: " + countMajor);
+  console.log("countMinor: "+ countMinor);
+  fs.writeFileSync('Changes.json', JSON.stringify(temp,undefined,2));
+
+  //console.log(keys);
+  //console.log(result[2]["Primary Curr Minor 1 Name"]);
+  //console.log(result);
   });
 
 
+//-------------------------------------------------------------------------------------
 
-
+  // var temp = [];
+  // var t;
+  // let query1 = "select * from BTT_TEST.student_semester_data";
+  // db.query(query1, function (err, result, fields) {
+  //   if (err) throw err;
+  //   fs.writeFileSync('Tech_Discipline.json', JSON.stringify(result,undefined,2));
+  //   //let keys = Object.getOwnPropertyNames(result)    
+  //   for (let i =0; i<result.length; i++) {
+  //     if (result[i].UIN == result[i+1].UIN) {
+  //       console.log(result);
+  //     }
+  //   }  
+  // //console.log(keys);
+  // //console.log(result[2]["Primary Curr Minor 1 Name"]);
+  //   //console.log(result);
+  // });
